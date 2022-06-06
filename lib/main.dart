10c0f2fb315f15edd6132bf615d6889809ba9f97
Waseem Abbas <engr.waseemabbas8@gmail.com>
2022-06-06 19:07:56 +0500

    fix screen utils not initialized in release mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,26 @@ import 'package:badlo/presentation/core/route/pages.dart';
 import 'package:badlo/presentation/core/route/routes.dart';
 import 'package:badlo/presentation/core/theme/light_theme.dart';
 import 'package:badlo/presentation/core/utils/screen_util.dart';
-import 'package:badlo/presentation/core/values/colors.dart';
 import 'package:badlo/presentation/page/onboarding/onboarding_bindings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:dio/dio.dart';
 
-void main() {
+void main() async {
   _initDependencies();
-  runApp(const MyApp());
-}
-
-_initDependencies() {
-  Get.lazyPut(() => Dio(), fenix: true);
+  await Future.delayed(const Duration(microseconds: 500));
   ScreenUtil.init(allowFontScaling: true);
-
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarBrightness: Brightness.light,
     statusBarIconBrightness: Brightness.dark, // transparent status bar
   ));
+  runApp(const MyApp());
+}
+
+_initDependencies() {
+  Get.lazyPut(() => Dio(), fenix: true);
 }
 
 class MyApp extends StatelessWidget {
